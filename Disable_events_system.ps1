0ee4097302a8a69b26de1b2c9bdf88b3309b2ba8
XunilK <48892193+XunilKoburax@users.noreply.github.com>
2023-09-04 23:53:27 -0600

    Create Disable_events_system.ps1
    make to deault seting the events on the server, to make efective the changes reboot the systems

---
+++ Disable_events_system.ps1
@@ -0,0 +1,3 @@
+auditpol /set /subcategory:"Application Generated" /success:disable /failure:disable
+auditpol /set /subcategory:"Other Object Access Events" /success:disable /failure:disable
+auditpol /set /subcategory:"Filtering Platform Packet Drop" /success:disable /failure:disable
